db01c5fc6c43672a3f8383d47945b6c5a0f97094
naoto.t <naoto.takaragaki@gmail.com>
2018-05-02 16:39:07 +0900

    Userに対するテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :login_id, presence: true, length: { maximum: 50 }
+  validates :login_id, presence: true, length: { maximum: 30 }
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,8 +9,15 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
-  test "name should be present" do
+  test "ログインIDが空でないこと" do
     @user.login_id = "     "
+    assert_not @user.valid?
+  end
+
+  test "ログインIDが30字以下であること" do
+    @user.login_id = "a" * 30
+    assert @user.valid?
+    @user.login_id = "a" * 31
     assert_not @user.valid?
   end
 end
