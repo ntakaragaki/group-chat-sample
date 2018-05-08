ef43902db63d816f0f9d62119bc56fbfb978137e
naoto.t <naoto.takaragaki@gmail.com>
2018-05-08 18:01:25 +0900

    ログイン画面まで実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(login_id: params[:session][:login_id])
+    if user && user.authenticate(params[:session][:password])
+      # ログイン処理
+      session[:user_id] = user.id
+      render 'chat'
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 end
