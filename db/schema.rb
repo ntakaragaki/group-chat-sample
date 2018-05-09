ab99fcca96748644c53d12918d01ea6c71589425
naoto.t <naoto.takaragaki@gmail.com>
2018-05-09 09:48:41 +0900

    Groupモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502065619) do
+ActiveRecord::Schema.define(version: 20180509004438) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login_id"
