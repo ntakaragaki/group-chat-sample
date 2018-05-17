010e258cad4951eab7437ae20668e463aedac3e6
naoto.t <naoto.takaragaki@gmail.com>
2018-05-17 18:46:59 +0900

    他ユーザーの発言に対してスタイルを追加

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,6 @@
 class ChatsController < ApplicationController
   def index
+    @current_user = User.find(session[:user_id])
     @group = Group.find(params[:group_id])
     @chats = @group.chats
     @users = @group.users
