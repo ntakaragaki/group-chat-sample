cecf2fba1f26b97f1ba7b93e6016087df6e13a3f
naoto.t <naoto.takaragaki@gmail.com>
2018-05-27 16:28:34 +0900

    PR指摘事項に対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,3 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
-
-window.addEventListener('load', () => {
-  var form = document.getElementById('message_form');
-  form.onsubmit = () => {
-    var text = document.getElementById('chat_input').value;
-    var user_login_id = document.getElementById('chat_user_login_id').value;
-    var group_id = document.getElementById('chat_group_id').value;
-    var chat = { text: text, user_login_id: user_login_id, group_id: group_id };
-    App.chat.put_message(chat);
-    form.reset();
-    return false;
-  }
-});
