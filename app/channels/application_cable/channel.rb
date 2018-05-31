890e58e30965f2f8b228617a0224fc68bb0223ac
naoto.t <naoto.takaragaki@gmail.com>
2018-05-31 19:22:12 +0900

    環境変数でAPI Keyを指定する

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,6 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    API_DIALOGUE_URL = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY=" << ENV["DOCOMO_API_KEY"]
+    API_REGISTER_URL = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/registration?APIKEY=" << ENV["DOCOMO_API_KEY"]
   end
 end
